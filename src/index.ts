72b5c7d17826b75943fec4b76ba44bdbe3798514
brendacoolen <92064251+brendacoolen@users.noreply.github.com>
2021-10-30 19:42:18 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+let tabla:string=document.getElementById
+let btn = document.getElementById("btn");
+btn.addEventListener("click", () => {
+function tabladel(numerodetabla :number) {
+  let Rango: number;
+   Rango=0;
+   for (let Rango= 1; Rango<=9; Rango++) {
+        let multiplicacion: number=rango*numerodetabla
+   }
+  if (tabla.value=== A|| tabla.value===B|| tabla.value===C|| tabla.value===D) {
+tabladel(9)
+      console.log ("tabladel")
+  } else {
+    console.log("invalido.Ingrese A B C o D")
+  }
